053e4ad4d129e843b1df28ad13a91d26249c4633
KremerStas <69371510+KremerStas@users.noreply.github.com>
2020-11-22 22:36:03 +0300

    Update settings.py

--- scraping_service/settings.py
+++ scraping_service/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '798be-@wlz$uyp=f^=*%a!cj&gx&5buuj$z51ucv-41=y9z4kw'
+SECRET_KEY = 'KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
